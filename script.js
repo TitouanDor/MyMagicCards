8b67e8888d069761c65032462d2fbf454631b062
Titouan Dorier <titouan.dorier@outlook.fr>
2023-03-04 17:37:21 +0100

    ajout boutton trie couleur

--- script.js
+++ script.js
@@ -1,6 +1,80 @@
-
 function whitebutton() {
+  $(".bluecard").hide();
+  $(".redcard").hide();
+  $(".blackcard").hide();
+  $(".greencard").hide();
+  $(".incolcard").hide();
+  $(".multicard").hide();
+  $(".whitecard").show();
+}
+
+function bluebutton() {
+  $(".bluecard").show();
+  $(".redcard").hide();
+  $(".blackcard").hide();
+  $(".greencard").hide();
+  $(".incolcard").hide();
+  $(".multicard").hide();
   $(".whitecard").hide();
-  /*$(".whitecard").show();*/
+}
+
+function redbutton() {
+  $(".bluecard").hide();
+  $(".redcard").show();
+  $(".blackcard").hide();
+  $(".greencard").hide();
+  $(".incolcard").hide();
+  $(".multicard").hide();
+  $(".whitecard").hide();
+}
+
+function blackbutton() {
+  $(".bluecard").hide();
+  $(".redcard").hide();
+  $(".blackcard").show();
+  $(".greencard").hide();
+  $(".incolcard").hide();
+  $(".multicard").hide();
+  $(".whitecard").hide();
+}
+
+function greenbutton() {
+  $(".bluecard").hide();
+  $(".redcard").hide();
+  $(".blackcard").hide();
+  $(".greencard").show();
+  $(".incolcard").hide();
+  $(".multicard").hide();
+  $(".whitecard").hide();
+}
+
+function incolbutton() {
+  $(".bluecard").hide();
+  $(".redcard").hide();
+  $(".blackcard").hide();
+  $(".greencard").hide();
+  $(".incolcard").show();
+  $(".multicard").hide();
+  $(".whitecard").hide();
+}
+
+function multibutton() {
+  $(".bluecard").hide();
+  $(".redcard").hide();
+  $(".blackcard").hide();
+  $(".greencard").hide();
+  $(".incolcard").hide();
+  $(".multicard").show();
+  $(".whitecard").hide();
+}
+
+function allbutton() {
+  $(".bluecard").show();
+  $(".redcard").show();
+  $(".blackcard").show();
+  $(".greencard").show();
+  $(".incolcard").show();
+  $(".multicard").show();
+  $(".whitecard").show();
 }
   
